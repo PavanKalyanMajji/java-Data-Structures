5bdb8c33ca358d10e274904c2abc5d73ff7040ac
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-06-18 19:25:13 +0530

    selection sort modified

--- src/com/tcs/datastructures/arraysorting/SelectionSorting.java
+++ src/com/tcs/datastructures/arraysorting/SelectionSorting.java
@@ -3,26 +3,28 @@ package com.tcs.datastructures.arraysorting;
 import java.util.Arrays;
 
 class SelectionSortingExample {
+//	{3,2,1}
 	public static int[] selectionSort(int[] array,int length) {
 		int minIndex=0;
 		int temp=0;
 		for(int i=0;i<length-1;i++) {
-			minIndex=i;
+			minIndex=i;//1
 			for(int j=i+1;j<length;j++) {
 				if(array[minIndex]>array[j]) {
 					minIndex=j;
 				}
 			}
-			temp=array[minIndex];
-			array[minIndex]=array[i];
-			array[i]=temp;
+			temp=array[minIndex];//1
+			array[minIndex]=array[i];//3
+			array[i]=temp;//1
 		}
 		return array;
 	}
 }
 public class SelectionSorting {
 	public static void main(String[] args) {
-		int[] array= {25,5,10,45,90,15,93,17,1,18};
-		System.out.println(Arrays.toString(SelectionSortingExample.selectionSort(array, array.length)));
+		int[] array= new int[]{3,2,1};
+		array=SelectionSortingExample.selectionSort(array, array.length);
+		System.out.println(Arrays.toString(array));
 	}
 }
